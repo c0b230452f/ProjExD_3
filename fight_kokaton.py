592a2e0bb9d4cc767b65c93bc6e7b204220d4b76
c0b23045 <c0b230452f@edu.teu.ac.jp>
2024-10-08 14:39:36 +0900

    練習３：ビームを撃ち落としたら喜ぶエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,9 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 # ビームと爆弾が衝突したら、両方を消滅させる
                 beam, bomb = None, None
+                bird.change_img(6, screen)
+                pg.display.update()
+                time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)  # birdの更新
